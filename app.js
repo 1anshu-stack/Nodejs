cb98428c888a28a49fd9035448a79df9809640ed
1anshu-stack <anshuu424@gmail.com>
2024-11-18 14:47:08 +0530

    node server update

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>hello from my node.js server!</h1></body>')
+    res.write('</html>')
+    res.end();
 });
 
 server.listen(3000);
