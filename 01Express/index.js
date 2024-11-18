a0425c4285c75ad306cad0a84df9cb79ca0b885f
1anshu-stack <anshuu424@gmail.com>
2024-11-18 17:37:56 +0530

    express body-parser

--- 01Express/index.js
+++ 01Express/index.js
@@ -1,18 +1,24 @@
 const express = require('express')
-const http = require('http')
+const bodyparser = require('body-parser')
+
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware...')
-    next();
+app.use(bodyparser.urlencoded({extended:false}))
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`<form action="/product" method="POST"><input type="text" name="name" value="John"><button type="submit">Submit</button></form>`)
 })
 
-app.use((req, res, next) => {
-    console.log("in another middleware")
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    // console.log("in another middleware")
     res.send(`<h1>hello from express</h1>`)
 })
 
 
-const server = http.createServer(app)
-server.listen(3000)
+app.listen(3000)
