538b627cdc1008b93df172501aaac9759a5a8907
1anshu-stack <anshuu424@gmail.com>
2024-11-18 18:20:27 +0530

    serve html file

--- 01Express/routes/shop.js
+++ 01Express/routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router();
 
-router.use('/', (req, res, next) => {
-    res.send(`<h1>hello from express</h1>`)
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
 })
 
 module.exports = router;
